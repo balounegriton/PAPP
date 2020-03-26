a409730014e35d0822d72fb226257f72bdfdc2b8
balounegriton <provostnoe@gmail.com>
2020-03-26 16:01:41 -0400

    Updates

---
+++ precache-manifest.86e8514ffe7f3739488e9b30137ccfe4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "665f92b2ff4bb12460ec1cef6db63afc",
+    "url": "/PAPP/index.html"
+  },
+  {
+    "revision": "eb795168b56745b3d4d7",
+    "url": "/PAPP/static/css/main.e9069255.chunk.css"
+  },
+  {
+    "revision": "ec8e7dd7e3f603b5ea01",
+    "url": "/PAPP/static/js/2.cd13a569.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PAPP/static/js/2.cd13a569.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb795168b56745b3d4d7",
+    "url": "/PAPP/static/js/main.8dad0495.chunk.js"
+  },
+  {
+    "revision": "32cffc3c179a0c3fa952",
+    "url": "/PAPP/static/js/runtime-main.b5e2d86f.js"
+  },
+  {
+    "revision": "2eb9100226c5e3c874b905dbaa0cfe2b",
+    "url": "/PAPP/static/media/book.2eb91002.jpg"
+  }
+]);
