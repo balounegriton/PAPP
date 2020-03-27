aafb5b3ceb75f3f27cc7df088c387a5ebd4a7f76
balounegriton <provostnoe@gmail.com>
2020-03-27 11:40:10 -0400

    simplification

--- src/App.js
+++ src/App.js
@@ -1,55 +1,21 @@
 import React from 'react';
 import './App.css';
 
-import Anim1 from "./animation/Amin1.js";
-import Anim2 from "./animation/Amin2.js";
+import Anim from "./animation/Anim.js";
 import book from './book.jpg'
-
-
 
 
 function App() {
   return (
-
-
     <div className="App">
-
       <div className="container">
         <img src={book} alt="img" className="Z-index" />
         <img src={book} alt="img" className="Z-index" />
         <img src={book} alt="img" />
-        <div className="anim1">
-          <Anim1 />
-        </div >
+        <div className="anim">
+          <Anim />
+        </div > 
       </div>
-
-
-      <div className="container">
-        <img src={book} alt="img" className="Z-index" />
-        <img src={book} alt="img" className="Z-index" />
-        <img src={book} alt="img" />
-      </div>
-
-
-      <div className="container">
-        <img src={book} alt="img" className="Z-index" />
-        <img src={book} alt="img" className="Z-index" />
-        <img src={book} alt="img" />
-        <div className="anim1">
-          <Anim1 />
-        </div >
-
-      </div>
-      <div className="container">
-        <img src={book} alt="img" className="Z-index" />
-        <img src={book} alt="img" className="Z-index" />
-        <img src={book} alt="img" />
-        <div className="anim2">
-          <Anim2 />
-        </div >
-      </div>
-     
-    
     </div>
   );
 }
--- src/animation/Anim.js
+++ src/animation/Anim.js
@@ -1,20 +1,21 @@
 import React from 'react'
 import ReactBodymovin from 'react-bodymovin'
-import animation from './anim2.json'
- 
-const App = () => {
+import animation from './anim.json'
+
+
+const Anim = () => {
   const bodymovinOptions = {
     loop: true,
     autoplay: true,
     prerender: true,
     animationData: animation
   }
- 
+
   return (
     <div>
       <ReactBodymovin options={bodymovinOptions} />
     </div>
   )
 }
- 
-export default App
+
+export default Anim
